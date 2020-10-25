eaf76dde48e5a6677abc8302cda89e318a86d7ad
legendbabs <tmsalami@student.lautech.edu.ng>
2020-10-25 18:07:08 +0100

    Added Python Script

---
+++ script.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, school, age):
+        self.name = name
+        self.school = school
+        self.age = age
+
+
+student1 = Student('Tunde Muhamed', 'LAUTECH', 29)
+student2 = Student('Shina Onilude', 'FUT-Minna', 30)
